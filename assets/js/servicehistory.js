15c9b3d1e0fa2b8ce4e5d4c258b3bb73afb5fc5f
Shubham-Padiya <shubhamp17825@gmail.com>
2022-01-11 22:22:17 +0530

    table_responsive

--- assets/js/servicehistory.js
+++ assets/js/servicehistory.js
@@ -1,30 +1,36 @@
-
-$(document).ready(function() {
-    $('#servicehistory').DataTable( {
-      searching:false,
-      info:false,
-      dom: '<"float-left"B><"float-right"f>rt<"row"<"col-sm-4"l><"col-sm-4"i><"col-sm-4"p>>',
-      "sPaginationType":"full_numbers",
-      language: {
-        paginate: {
-          first:'<img src="assets/images/left_play.png">',
-          next: '<img src="assets/images/keyboard-right-arrow-buttonn.png">',
-          previous: '<img src="assets/images/keyboard-left-arrow-button.png">',
-          last:'<img src="assets/images/right_play.png">'
-        },
+$(document).ready(function () {
+  $("#servicehistory").DataTable({
+    searching: false,
+    info: false,
+    dom: '<"float-left"B><"float-right"f>rt<"row"<"col-sm-4"l><"col-sm-4"i><"col-sm-4"p>>',
+    sPaginationType: "full_numbers",
+    language: {
+      paginate: {
+        first: '<img src="assets/images/left_play.png">',
+        next: '<img src="assets/images/keyboard-right-arrow-buttonn.png">',
+        previous: '<img src="assets/images/keyboard-left-arrow-button.png">',
+        last: '<img src="assets/images/right_play.png">',
       },
-        responsive: {
-            details: {
-                display: $.fn.dataTable.Responsive.display.modal( {
-                    header: function ( row ) {
-                        var data = row.data();
-                        return 'Details for '+data[0]+' '+data[1];
-                    }
-                } ),
-                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
-                    tableClass: 'table'
-                } )
-            }
-        }
-    } );
-  } );
+    },
+    responsive: {
+      details: {
+        display: $.fn.dataTable.Responsive.display.modal({
+          header: function (row) {
+            var data = row.data();
+            return "Details for " + data[0] + " " + data[1];
+          },
+        }),
+        renderer: $.fn.dataTable.Responsive.renderer.tableAll({
+          tableClass: "table",
+        }),
+      },
+    },
+  });
+  $(".buttons-pdf").hide();
+  $(".buttons-copy").hide();
+  $(".buttons-csv").hide();
+});
+
+$("#export").on("click", function () {
+  $(".buttons-pdf").trigger("click");
+});
